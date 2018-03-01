a30c79a7cf373b846270321fdc3a64461b1c4ebd
Josh Peak <neozenith.dev@gmail.com>
2018-03-02 10:02:27 +1100

    Worked through D3 bar chart demo

--- plot/plot.js
+++ plot/plot.js
@@ -1,43 +1,84 @@
 console.log('Plot ENTER');
 
 var data = [4, 8, 15, 16, 23, 42];
+var margin = { top: 20, right: 30, bottom: 50, left: 40 },
+	width = 960 - margin.left - margin.right,
+	height = 500 - margin.top - margin.bottom;
 
-var width = 420,
-	barHeight = 20;
+// Define a linear scale for output geometry
+var x = d3.scale.ordinal().rangeRoundBands([0, width], 0.1);
 
-var x = d3.scale
-	.linear()
-	.domain([0, d3.max(data)])
-	.range([0, width]);
+var y = d3.scale.linear().range([height, margin.top]);
+
+// Defince chart axes
+var xAxis = d3.svg
+	.axis()
+	.scale(x)
+	.orient('bottom');
+
+var yAxis = d3.svg
+	.axis()
+	.scale(y)
+	.orient('left');
 
 var chart = d3
 	.select('#chart')
-	.attr('width', width)
-	.attr('height', barHeight * data.length);
-
-var bar = chart
-	.selectAll('g')
-	.data(data)
-	.enter()
+	.attr('width', width + margin.left + margin.right)
+	.attr('height', height + margin.top + margin.bottom)
 	.append('g')
-	.attr('transform', function(d, i) {
-		return 'translate(0,' + i * barHeight + ')';
-	});
+	.attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
 
-bar
-	.append('rect')
-	.attr('width', x)
-	.attr('height', barHeight - 1);
+d3.csv('data.csv', type, function(error, data) {
+	//Mapp the data domain to linear scale
+	x.domain(
+		data
+			.map(function(d) {
+				return d.date;
+			})
+			.sort()
+	);
 
-bar
-	.append('text')
-	.attr('x', function(d) {
-		return x(d) - 3;
-	})
-	.attr('y', barHeight / 2)
-	.attr('dy', '.35em')
-	.text(function(d) {
-		return d;
-	});
+	y.domain([
+		d3.min(data, function(d) {
+			return parseFloat(d.value);
+		}),
+		d3.max(data, function(d) {
+			return parseFloat(d.value);
+		})
+	]);
+
+	chart
+		.append('g')
+		.attr('class', 'x axis')
+		.attr('transform', 'translate(0,' + height + ')')
+		.call(xAxis);
+
+	chart
+		.append('g')
+		.attr('class', 'y axis')
+		.call(yAxis);
+
+	chart
+		.selectAll('.bar')
+		.data(data)
+		.enter()
+		.append('rect')
+		.attr('class', 'bar')
+		.attr('x', function(d) {
+			return x(d.date);
+		})
+		.attr('y', function(d) {
+			return y(d.value);
+		})
+		.attr('height', function(d) {
+			return height - y(d.value);
+		})
+		.attr('width', x.rangeBand());
+});
+
+function type(d) {
+	d.value = +d.value; // coerce to number
+	return d;
+}
 
 console.log('Plot EXIT');
