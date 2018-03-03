e8e7edbd404a20df5c117b7a0a83c980daa6c462
Josh Peak <neozenith.dev@gmail.com>
2018-03-04 08:13:35 +1100

    scraping entire range

--- articli-runner.sh
+++ articli-runner.sh
@@ -5,7 +5,7 @@ SAMPLE=8
 
 while [ 1 == 1 ] ;
 do
-	./articli -u http://www.theherald.com.au/story -r 374000..375000 -s $SAMPLE
+	./articli -u http://www.theherald.com.au/story -r 144..5244000 -s $SAMPLE
 	echo "Waiting for $TIMEOUT seconds..."
 	sleep $TIMEOUT
 	echo `date`
